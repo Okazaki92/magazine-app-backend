af09ee0e4ed4a4839dbad97631e87e31bf2f3368
Okazaki92 <vincentslominski@gmail.com>
2023-10-08 22:29:04 +0200

    fix

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const userRouter = require("./routes/userRoutes");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.static("public"));
+app.use(cors());
 
 app.use("/users", userRouter);
 app.get("/", (req, res) => res.send("Welcome"));
