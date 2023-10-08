15ea57fce3d906d330085704daaf793319f1e0c5
Okazaki92 <vincentslominski@gmail.com>
2023-10-08 22:14:36 +0200

    fix/mogo

--- server.js
+++ server.js
@@ -2,20 +2,17 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_DB, {
-      dbName: "Magazine-App",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connect to MongoDB successfully");
-  } catch (error) {
-    console.log("Connect failed " + error.message);
-  }
-};
+mongoose.connect(process.env.MONGO_DB, {
+  dbName: "Magazine-App",
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const PORT = process.env.PORT || 3000;
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongo"));
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
