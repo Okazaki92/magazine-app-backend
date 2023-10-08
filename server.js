36e1db2e062d5724d96bc666e37e3e35bb67917c
Okazaki92 <vincentslominski@gmail.com>
2023-10-08 21:04:46 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { error } = require("console");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_DB, {
+  dbName: "Magazine-App",
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongo"));
+
+app.use(express.json());
+
+app.listen(3000, () => console.log("Server run at http://localhost:3000"));
+
+app.get("/", (req, res) => res.json({ message: "Welcome" }));
